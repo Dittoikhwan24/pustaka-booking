3d325d8d76977c863bfaeb0872042b5ab2485550
Dittoikhwan24 <101235014+Dittoikhwan24@users.noreply.github.com>
2023-09-30 15:12:33 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama Saya Ditto Ikhwan Zufar Althoof 
+            Saya Tinggal Di Jakarta
+            Olahraga Yang Saya Sukai Adalah Bola";
+    }
+}
